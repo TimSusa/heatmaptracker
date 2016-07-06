b2b8faee66f393b53b56e0373eecf1d95fc7f861
TimSusa <timsusa@altus-insight.de>
2016-07-06 13:30:21 +0200

    HOD-677

--- heatmaptracker.js
+++ heatmaptracker.js
@@ -1,8 +1,9 @@
-(function snippet(w, d) {
+(function snippet (w, d) {
     'use strict';
 
     var intervalFunc;
     var objToSend = {}; // The object to send
+    var isAllowedToSend = true;
 
     addLoadEvent(init);
 
@@ -12,45 +13,16 @@
       intervalFunc = setInterval(sendEvent, 100);
     }
 
-    function sendEvent () {
-      var url = 'https://api.realtimeheatmap.com';
-      var token = '7294232472';
-      var collectionUrl = [url, '1', 'channels', 'heatmapchannel1' ].join('/');
-      var xhttp;
-      if (w.XMLHttpRequest) {
-        xhttp = new XMLHttpRequest();
+    function addLoadEvent (func) {
+      var oldonload = w.onload;
+      if (typeof w.onload !== 'function') {
+        w.onload = func;
       } else {
-        // code for IE6, IE5
-        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        w.onload = function() {
+          oldonload();
+          func();
+        };
       }
-      // true for its third parameter to indicate that the request should be handled asynchronously
-      xhttp.open("POST", collectionUrl, true);
-      xhttp.setRequestHeader("Content-Type", "application/json");
-      xhttp.setRequestHeader("X-AUTH-TOKEN", token);
-      xhttp.setRequestHeader("Accept", "application/json");
-
-      var jsonBlob = JSON.stringify(objToSend);
-      if (Object.keys(objToSend).length !== 0) {
-        xhttp.send(jsonBlob);
-      }
-    }
-
-    function trackCoord (blob) {
-      var maxX = d.body.clientWidth;
-      var maxY = d.body.clientHeight;
-      objToSend = {
-        view: separateViewFromUrl(),
-        x: blob.pageX,
-        y: blob.pageY,
-        xMax: maxX,
-        yMax: maxY,
-        timestamp: new Date().getTime()
-      };
-      // console.log('view: ', objToSend.view);
-      // console.log('x: ', objToSend.x);
-      // console.log('y: ', objToSend.y);
-      // console.log('xMax: ', objToSend.xMax);
-      // console.log('yMax: ', objToSend.yMax);
     }
 
     function addEvent (elm, evType, fn, useCapture) {
@@ -65,15 +37,57 @@
       }
     }
 
-    function addLoadEvent (func) {
-      var oldonload = w.onload;
-      if (typeof w.onload !== 'function') {
-        w.onload = func;
+    function sendEvent () {
+      var url = 'https://api.realtimeheatmap.com';
+      var token = w.token;
+      var collectionUrl = [url, '1', 'channels', 'heatmapchannel1' ].join('/');
+      var xhttp;
+      if (w.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
       } else {
-        w.onload = function() {
-          oldonload();
-          func();
-        };
+        // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState === 4 && xhttp.status !== 201) {
+          console.log('Error when sending data...', xhttp.responseText, ' ', xhttp.status);
+          isAllowedToSend = false;
+        }
+      };
+
+      // true for its third parameter to indicate that the request should be handled asynchronously
+      xhttp.open("POST", collectionUrl, true);
+      xhttp.setRequestHeader("Content-Type", "application/json");
+      xhttp.setRequestHeader("X-AUTH-TOKEN", token);
+      xhttp.setRequestHeader("Accept", "application/json");
+
+      var jsonBlob = JSON.stringify(objToSend);
+      if ((Object.keys(objToSend).length !== 0) && isAllowedToSend) {
+        xhttp.send(jsonBlob);
+      }
+    }
+
+    function trackCoord (blob) {
+      var maxX = d.body.clientWidth;
+      var maxY = d.body.clientHeight;
+      objToSend = {
+        view: separateViewFromUrl(),
+        x: blob.pageX,
+        y: blob.pageY,
+        xMax: maxX,
+        yMax: maxY,
+        timestamp: new Date().getTime(),
+        userId: w.userId
+      };
+
+      if (w.isDebug) {
+        console.log('view: ', objToSend.view);
+        console.log('x: ', objToSend.x);
+        console.log('y: ', objToSend.y);
+        console.log('xMax: ', objToSend.xMax);
+        console.log('yMax: ', objToSend.yMax);
+        console.log('userId: ', objToSend.userId);
       }
     }
 
